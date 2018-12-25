d9c9b7e7808197ac4aaf0ca035acaf22e2af6e7c
Darth Coder <00darthcoder00@gmail.com>
2018-12-25 14:16:19 +0200

    Update expense amount&date formatting

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
+
+//Format currency to EGP
+numeral.register('locale', 'eg', {
+    delimiters: {
+        thousands: ' ',
+        decimal: ','
+    },
+    abbreviations: {
+        thousand: 'k',
+        million: 'm',
+        billion: 'b',
+        trillion: 't'
+    },
+    ordinal : function (number) {
+        return number === 1 ? 'EGP' : 'L.E';
+    },
+    currency: {
+        symbol: 'EGP'
+    }
+});
+
+
 
 export const ExpenseListItem = ({id, description, amount, createdAt}) => (
     <div>
         <Link to={`edit/${id}`}><h3>{description}</h3></Link>
-        <p>{amount} - {createdAt}</p>
+        <p>{numeral(amount / 100).format('$0,0.00')} - 
+        {moment(createdAt).format('DD-MMMM-YYYY')}</p>
     </div>
 )
+
+numeral.locale('eg');
 
 export default ExpenseListItem;
