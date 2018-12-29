b4a35713bb8ebe9bf0150b089a7c1ba99a408e19
Darth Coder <00darthcoder00@gmail.com>
2018-12-29 17:20:20 +0200

    Set Database privacy and security

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -16,16 +16,20 @@ import database from '../../firebase/firebase'
 const middleWare = [thunk];
 const mockStore = configureStore(middleWare);
 
+// fake id and auth state for test purposes
+const fake_uid = 'xxyyzz123456';
+const defAuthState = {auth: {uid: fake_uid}};
+
 beforeEach((done) => {
     const expenseData = {};
     expenses.forEach(({id, description, note, amount, createdAt}) => {
         expenseData[id] = {description, note, amount, createdAt}
     });
-    database.ref('expenses').set(expenseData).then(() => done());
+    database.ref(`users/${fake_uid}/expenses`).set(expenseData).then(() => done());
 });
 
 test('should remove expenses data from database', (done) => {
-    const store = mockStore({});
+    const store = mockStore(defAuthState);
     const id = expenses[2].id
     store.dispatch(startRemoveExpense({id})).then(() => {
         const actions = store.getActions();
@@ -33,7 +37,7 @@ test('should remove expenses data from database', (done) => {
             type: 'REMOVE_EXPENSE',
             id
         });
-        return database.ref(`expenses/${id}`).once('value');
+        return database.ref(`users/${fake_uid}/expenses/${id}`).once('value');
         }).then((snapshot) => {
             expect(snapshot.val()).toBeFalsy();
             done();
@@ -51,7 +55,7 @@ test('should generate the removeExpense action object', () => {
 });
 
 test('should edit expenses data within database', (done) => {
-    const store = mockStore({});
+    const store = mockStore(defAuthState);
     const id = expenses[2].id
     const updates = {description: 'Something different and badass like me!'}
 
@@ -62,7 +66,7 @@ test('should edit expenses data within database', (done) => {
             id,
             updates
         });
-        return database.ref(`expenses/${id}`).once('value');
+        return database.ref(`users/${fake_uid}/expenses/${id}`).once('value');
         }).then((snapshot) => {
             expect(snapshot.val().description).toBe(updates.description);
             done();
@@ -92,7 +96,7 @@ test('should generate the addExpense action object', () => {
 });
 
 test('should add expense to database and store', (done) => {
-    const store = mockStore({});
+    const store = mockStore(defAuthState);
 
     const expenseDummyData = {
         description: 'Headphones',
@@ -110,7 +114,7 @@ test('should add expense to database and store', (done) => {
                 ...expenseDummyData
             }
         });
-        return database.ref(`expenses/${actions[0].expense.id}`).once('value');
+        return database.ref(`users/${fake_uid}/expenses/${actions[0].expense.id}`).once('value');
     }).then((snapshot) => {
         const value = snapshot.val();
         expect(value).toEqual(expenseDummyData);
@@ -119,7 +123,7 @@ test('should add expense to database and store', (done) => {
 });
 
 test('should add expense with default values to database and store', (done) => {
-    const store = mockStore({});
+    const store = mockStore(defAuthState);
 
     const defaultData = {
         description: '',
@@ -137,7 +141,7 @@ test('should add expense with default values to database and store', (done) => {
                 ...defaultData
             }
         });
-        return database.ref(`expenses/${actions[0].expense.id}`).once('value');
+        return database.ref(`users/${fake_uid}/expenses/${actions[0].expense.id}`).once('value');
     }).then((snapshot) => {
         const value = snapshot.val();
         expect(value).toEqual(defaultData);
@@ -154,7 +158,7 @@ test('should set expenses', () => {
 });
 
 test('should fetch expenses from database', (done) => {
-    const store = mockStore({});
+    const store = mockStore(defAuthState);
     store.dispatch(startSetExpenses()).then(() => {
         const actions = store.getActions();
         expect(actions[0]).toEqual({
