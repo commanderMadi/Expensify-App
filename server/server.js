bd5aace41ec36c0afb237eeb8e886b02beb9207b
Darth Coder <00darthcoder00@gmail.com>
2019-01-01 18:26:42 +0200

    Test ssl forced redirect for heroku

--- server/server.js
+++ server/server.js
@@ -1,15 +1,20 @@
 const path = require('path');
 const express = require('express');
+const sslRedirect = require('heroku-ssl-redirect');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
+app.use(sslRedirect([
+    'development',
+    'production'
+    ]));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
 });
 
 app.listen(port, () => {
-  console.log('Server is up!');
+  console.log('Server is up on port ', port);
 });
